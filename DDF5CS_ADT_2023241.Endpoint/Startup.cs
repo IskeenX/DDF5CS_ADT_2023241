2cd88d1276b00cda1f12c73df77b677c5cb7be3e
Iskender Degenbaev <degenisken@mail.ru>
2023-11-11 13:20:55 +0100

    Changed

--- DDF5CS_ADT_2023241.Endpoint/Startup.cs
+++ DDF5CS_ADT_2023241.Endpoint/Startup.cs
@@ -1,4 +1,5 @@
-﻿using DDF5CS_ADT_2023241.Repository;
+﻿using DDF5CS_ADT_2023241.Logic;
+using DDF5CS_ADT_2023241.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace DDF5CS_ADT_2023241.Endpoint
@@ -10,9 +11,14 @@ namespace DDF5CS_ADT_2023241.Endpoint
         {
             //Other configurations...
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+
+            services.AddScoped<IBrandRepository, BrandRepository>();
+            services.AddScoped<ICarModelRepository, CarModelRepository>();
+            services.AddScoped<IRentInstanceRepository, RentInstanceRepository>();
             
-            services.AddScoped
-            
+            services.AddScoped<IBrandLogic, BrandLogic>();
+            services.AddScoped<ICarModelLogic, CarModelLogic>();
+            services.AddScoped<IRentInstanceLogic, RentInstanceLogic>();
             
             //Other configurations...
         }
--- DDF5CS_ADT_2023241.Logic/CarModelRepository.cs
+++ DDF5CS_ADT_2023241.Logic/CarModelRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DDF5CS_ADT_2023241.Logic
 {
-    public class CarModelRepository
+    public class CarModelRepository : ICarModelRepository
     {
         private readonly AppDbContext _dbContext;
 
