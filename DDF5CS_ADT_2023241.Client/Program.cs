39160b4c07ba9840f773ffdbff621bd87fb05b0b
Iskender Degenbaev <degenisken@mail.ru>
2023-11-14 14:50:58 +0100

    Changed

--- DDF5CS_ADT_2023241.Client/Program.cs
+++ DDF5CS_ADT_2023241.Client/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace DDF5CS_ADT_2023241.Client
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DDF5CS_ADT_2023241.Client
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
+            {
+                // Add necessary services for your console application
+                services.AddTransient< IAPIInteractionService, APIInteractionService>();
+                // Add other services related to your application
+
+                // Perform API interactions in the Main method
+                using var serviceScope = services.BuildServiceProvider().CreateScope();
+                var apiInteractionService = serviceScope.ServiceProvider.GetRequiredService<IAPIInteractionService>();
+
+                /*// Example interaction with the API service
+                var result = myService.CallAPIMethod();
+                Console.WriteLine(result); // Display the result or handle it accordingly*/
+
+                // Example: Fetch data from API and display it in the console
+                var dataFromAPI = apiInteractionService.FetchDataFromAPI();
+                Console.WriteLine("Data from API:");
+                Console.WriteLine(dataFromAPI);
+
+            });
+    }
+
+    public interface IAPIInteractionService
+    {
+        string FetchDataFromAPI();
+    }
+
+    public class APIInteractionService : IAPIInteractionService
+    {
+        public string FetchDataFromAPI()
+        {
+            return "Data from the API";
         }
     }
 }
